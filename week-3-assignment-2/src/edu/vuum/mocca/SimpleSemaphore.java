65840038f0185825281d3a1f41d0217bf48c1889
Arnault <arnault.droz@gmail.com>
2014-06-09 16:00:14 +0200

    Implement SimpleSemaphore with try finally clause

--- week-3-assignment-2/src/edu/vuum/mocca/SimpleSemaphore.java
+++ week-3-assignment-2/src/edu/vuum/mocca/SimpleSemaphore.java
@@ -14,57 +14,93 @@ import java.util.concurrent.locks.Condition;
  *        just liked Java Semaphores.
  */
 public class SimpleSemaphore {
-    /**
-     * Define a ReentrantLock to protect the critical section.
-     */
-    // TODO - you fill in here
+	/**
+	 * Define a ReentrantLock to protect the critical section.
+	 */
+	// TODO - you fill in here
+	private final Lock lock;
 
-    /**
-     * Define a Condition that waits while the number of permits is 0.
-     */
-    // TODO - you fill in here
+	/**
+	 * Define a Condition that waits while the number of permits is 0.
+	 */
+	// TODO - you fill in here
+	private Condition condition;
+	/**
+	 * Define a count of the number of available permits.
+	 */
+	// TODO - you fill in here.  Make sure that this data member will
+	// ensure its values aren't cached by multiple Threads..
+	private volatile int counter;
 
-    /**
-     * Define a count of the number of available permits.
-     */
-    // TODO - you fill in here.  Make sure that this data member will
-    // ensure its values aren't cached by multiple Threads..
 
-    public SimpleSemaphore(int permits, boolean fair) {
-        // TODO - you fill in here to initialize the SimpleSemaphore,
-        // making sure to allow both fair and non-fair Semaphore
-        // semantics.
-    }
+	public SimpleSemaphore(int permits, boolean fair) {
+		// TODO - you fill in here to initialize the SimpleSemaphore,
+		// making sure to allow both fair and non-fair Semaphore
+		// semantics.
+		this.lock = new ReentrantLock(fair);
+		this.condition = lock.newCondition();
+		this.counter = permits;
+	}
 
-    /**
-     * Acquire one permit from the semaphore in a manner that can be
-     * interrupted.
-     */
-    public void acquire() throws InterruptedException {
-        // TODO - you fill in here.
-    }
+	/**
+	 * Acquire one permit from the semaphore in a manner that can be
+	 * interrupted.
+	 */
+	public void acquire() throws InterruptedException {
+		// TODO - you fill in here.
+		lock.lockInterruptibly();
+		try{
+			while(counter == 0){
+				condition.await();
+			}
+			counter--;
+		}
+		finally{
+			lock.unlock();
+		}
+	}
 
-    /**
-     * Acquire one permit from the semaphore in a manner that cannot be
-     * interrupted.
-     */
-    public void acquireUninterruptibly() {
-        // TODO - you fill in here.
-    }
+	/**
+	 * Acquire one permit from the semaphore in a manner that cannot be
+	 * interrupted.
+	 */
+	public void acquireUninterruptibly() {
+		// TODO - you fill in here.
+		lock.lock();
+		try{
+			while(counter == 0){
+				condition.awaitUninterruptibly();
+			}
+			counter--;
+		}
+		finally{
+			lock.unlock();
+		}
+	}
 
-    /**
-     * Return one permit to the semaphore.
-     */
-    void release() {
-        // TODO - you fill in here.
-    }
+	/**
+	 * Return one permit to the semaphore.
+	 */
+	void release() {
+		// TODO - you fill in here.
+		lock.lock();
+		try{
+			counter++;
+			if(counter != 0){
+				condition.signal();
+			}
+		}
+		finally{
+			lock.unlock();
+		}
+	}
 
-    /**
-     * Return the number of permits available.
-     */
-    public int availablePermits() {
-        // TODO - you fill in here by changing null to the appropriate
-        // return value.
-        return null;
-    }
+	/**
+	 * Return the number of permits available.
+	 */
+	public int availablePermits() {
+		// TODO - you fill in here by changing null to the appropriate
+		// return value.
+		return counter;
+	}
 }
